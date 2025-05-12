1fd7c1923c7f5aadd804f6a4d071562dbe3da90e
Shreshth Mohan <sremog@gmail.com>
2025-05-12 18:30:06 +0530

    Add new experiments: map-2, design-system, memory-map and logo files

---
+++ curl.sh
@@ -0,0 +1,16 @@
+curl 'https://cdn.teeinblue.com/users/13965/fonts/6426cf352f900.ttf' \
+  -H 'accept: */*' \
+  -H 'accept-language: en-US,en;q=0.9,hi;q=0.8,fr;q=0.7' \
+  -H 'cache-control: no-cache' \
+  -H 'origin: https://mrkyourmoment.com' \
+  -H 'pragma: no-cache' \
+  -H 'priority: u=0' \
+  -H 'referer: https://mrkyourmoment.com/' \
+  -H 'sec-ch-ua: "Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "macOS"' \
+  -H 'sec-fetch-dest: font' \
+  -H 'sec-fetch-mode: cors' \
+  -H 'sec-fetch-site: cross-site' \
+  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36' \
+  --output downloaded-font.ttf
